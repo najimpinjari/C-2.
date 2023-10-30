e8a30be7fa8880d94d78098a9fe5da06da6b3793
NAJIM PINJARI <143899549+najimpinjari@users.noreply.github.com>
2023-10-30 17:59:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_NullableType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nullable and non - non-nullable types //
+
+            // int = non nullable
+            // int? = nullable
+            string name1 = "jayesh";
+            Console.WriteLine(name1);
+
+            int? k = null;
+            int l = k ?? 2 ;
+            Console.WriteLine($" k : {k} l : {l} ");
+
+            int ? k2  = null; //or values 4
+            int l2 = k2 ?? 4 ;
+            Console.WriteLine($" k2 : {k2}  l2 : {l2} ");
+
+            string email = "jayeshmalache1@gmail.com";
+            string emailUppercase = email.ToUpper() ;   
+            Console.WriteLine($" email ; {email} emailUppercase : {emailUppercase } ")
+
+
+           
+
+          
+
+
+            Console.ReadLine();
+        }
+    }
+}
